641039760ba654dda2bd06343e11bd746b0dbe7c
zluca <LZheng@qubicaamf.com>
2025-10-01 11:40:28 +0200

    add odd even

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+Console.WriteLine("Hello, World!");
 
 int myInt = 10;
 string myString = "string";
@@ -7,3 +8,15 @@ bool myBool = true;
 Console.WriteLine(myInt);
 Console.WriteLine(myString);
 Console.WriteLine(myBool);
+
+Console.WriteLine("Insert a number:");
+int input = Convert.ToInt32(Console.ReadLine());
+
+if (input % 2 == 0)
+{
+    Console.WriteLine($"{input} is even");
+}
+else
+{
+    Console.WriteLine($"{input} is odd");
+}
