ebe42b15b669afa754162e4b4e3afbd60f3ca11d
zluca <LZheng@qubicaamf.com>
2025-10-01 11:25:27 +0200

    add hello world

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+
+int myInt = 10;
+string myString = "string";
+bool myBool = true;
+
+Console.WriteLine(myInt);
+Console.WriteLine(myString);
+Console.WriteLine(myBool);
