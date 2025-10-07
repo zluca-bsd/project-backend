92b5e8c629f4827636e759beccb6ab0c7e6449c5
zluca <LZheng@qubicaamf.com>
2025-10-07 10:36:43 +0200

    Migration

---
+++ Migrations/20251007075317_UpdateOrders.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace project_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOrders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "Orders",
+                newName: "BookId");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Books",
+                newName: "Title");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Books",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Books");
+
+            migrationBuilder.RenameColumn(
+                name: "BookId",
+                table: "Orders",
+                newName: "ProductId");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Books",
+                newName: "Name");
+        }
+    }
+}
